e675594077de3cf94d7edd2ed2673b4cfa5fc955
pambaka <pambaka@ya.ru>
2024-04-05 09:39:06 +0200

    feat: getCurrentFunctionName, getFunctionBody, getArgumentsCount are implemented

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -32,8 +32,12 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (func) {
+    return func.toString();
+  }
+
+  return '';
 }
 
 /**
@@ -50,8 +54,14 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const argumentsCount = [];
+
+  funcs.forEach((func) => {
+    argumentsCount.push(func.length);
+  });
+
+  return argumentsCount;
 }
 
 /**
