728a06954f0b7f9c60ccc429887a320b2baca8ec
pambaka <pambaka@ya.ru>
2024-04-08 17:20:43 +0200

    feat: memoize is implemented

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -135,8 +135,16 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = new Map();
+
+  function getValue(n) {
+    if (!cache.has(n)) cache.set(n, func(n));
+
+    return cache.get(n);
+  }
+
+  return getValue;
 }
 
 /**
