d2a964fea3145b2bcea068dc13945b39c1657b78
pambaka <pambaka@ya.ru>
2024-04-09 10:49:40 +0200

    feat: partialUsingArguments is applied

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -217,8 +217,14 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    if ([...args1, ...args2].length >= fn.length) {
+      return fn(...args1, ...args2);
+    }
+
+    return partialUsingArguments(fn, ...args1, ...args2);
+  };
 }
 
 /**
