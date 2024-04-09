f5d2e5ff6579d27bc2aecb1574e626039601c8f3
pambaka <pambaka@ya.ru>
2024-04-09 11:03:35 +0200

    feat: getIdGeneratorFunction is implemented

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -244,8 +244,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom - 1;
+
+  return () => {
+    counter += 1;
+
+    return counter;
+  };
 }
 
 module.exports = {
